96a01385402bada858ad100e9cc735619bd629fa
sunyuan <614559609@qq.com>
2025-06-18 09:46:17 +0800

    2025.6.18 产品送检单和工序检验单有一点问题

--- src/sale-out/sale_out.py
+++ src/sale-out/sale_out.py
@@ -12,8 +12,8 @@ class SaleOut:
     def auto_sale_out_code(self):
         """获取出库订单的自动编号"""
         relative_url ="admin-api/system/autocode/get/PRODUCTSALSE_CODE"
-        productsalse_code = self.api.send_get_direct(relative_url)
-        return productsalse_code
+        product_salse_code = self.api.send_get_direct(relative_url)
+        return product_salse_code
 
     def process_reporting_payload_get(self, code):
         """根据销售订单编号，获取负载主体部分"""
@@ -23,7 +23,8 @@ class SaleOut:
         return data["father"][0], data["children"]
 
     def client_info_get(self, name):
-        """根据客户名称查员工信息，返回用户id"""
+        # 这个有的订单会自带客户信息
+        """根据客户名称查客户信息，返回用户id"""
         name_code = quote(name)
         relative_url = f"admin-api/mes/md/client/list?pageNum=1&pageSize=10&clientName={name_code}"
         response = self.api.send_get_direct(relative_url)
@@ -32,18 +33,30 @@ class SaleOut:
             raise ValueError(f"未找到名为 {name} 的客户，请确认客户是否存在")
 
         client_info = response["rows"][0]  # 取第一个匹配结果
-        return client_info["userId"],client_info["userName"]
+        return client_info["clientId"],client_info["clientName"],client_info["clientCode"]
 
     def sale_out_add(self):
         """销售管理-销售订单-出库"""
         relative_url = "admin-api/mes/wm/productsalse/addNew"
-
+        data_main,data_list = self.process_reporting_payload_get("SAL2025241")
+        updated_data_list = []
+        for item in data_list:
+            new_item = item.copy()
+            # 这里直接把出库数量等于未收货数量，后面再改
+            new_item["quantitySalse"] = item["unreceivedGoods"]
+            updated_data_list.append(new_item)
+        # salseCode是出库单据号，salesCode是销售订单号，注意分辨
         payload = {
+
+            **data_main,
+            "list" :updated_data_list,
             "salseCode": self.auto_sale_out_code(),
-            "salseData" :"2025-06-17 17:45:48"
+            "salseDate": "2025-06-17 17:45:48",
+            "warehouseInfo" : [],
 
 
         }
+        print(payload)
 
         # 发送 POST 请求（JSON 格式）
         response = self.api.send_post_direct(relative_url, payload)
@@ -89,7 +102,7 @@ class SaleOut:
         }
         return  payload
 
-    def processInstance_cancleFlow(self, business_id, payload):
+    def process_instance_cancel_flow(self,  payload):
         "销售管理-销售订单明细--批量审批"
         relative_url = "admin-api/oa/myTask/commitTask"
 
@@ -105,5 +118,5 @@ if __name__ == "__main__":
     # 调用 订单新增，查询订单，审核订单 方法
     business_id = sale_out_instance.sale_out_add()
     payload = sale_out_instance.commit_task_by_business_id(business_id)
-    response_code = sale_out_instance.processInstance_cancleFlow(business_id, payload)
+    response_code = sale_out_instance.process_instance_cancel_flow(payload)
     print(f"审批返回状态码：{response_code}")
